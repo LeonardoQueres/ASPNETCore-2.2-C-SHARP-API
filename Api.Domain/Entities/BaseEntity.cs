29197d12d91f55aaee2e99f4ac0da38e45178c18
Leonardo Queres <leonardoqueres@hotmail.com>
2019-09-13 01:58:03 -0300

    Initial commit

---
+++ Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Api.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        private DateTime? _CreateAt;
+
+        public DateTime? CreateAt
+        {
+            get { return _CreateAt; }
+            set { _CreateAt = (value == null ? DateTime.UtcNow : value); }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+
+    }
+}
